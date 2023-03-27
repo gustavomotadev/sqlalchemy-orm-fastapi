2f57dcb85ae69a621e51a01c18edc290c42e55bd
gustavomotadev <gustavomotadev@gmail.com>
2023-03-27 14:05:45 -0300

    RepositorioBase is now abstract

--- src/locacao/repositorios/repositorio_base.py
+++ src/locacao/repositorios/repositorio_base.py
@@ -2,21 +2,25 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.engine import Engine
 from typing import List
+from abc import ABC, abstractmethod
 from locacao.modelos.modelo_base import ModeloBase
 
-class RepositorioBase(object):
+class RepositorioBase(ABC):
     
+    @abstractmethod
     def __init__(self, engine: Engine, modelo: ModeloBase) -> None:
         super().__init__()
         self.engine = engine
         self.modelo = modelo
 
+    @abstractmethod
     def listar_todos(self) -> List[ModeloBase]:
         with orm.Session(self.engine, expire_on_commit=False) as session:
             listados = session.execute(
                 sa.select(self.modelo)).scalars().all()
         return listados
     
+    @abstractmethod
     def inserir(self, **kwargs) -> ModeloBase:
         criado = self.modelo(**kwargs)
         with orm.Session(self.engine, expire_on_commit=False) as session:
@@ -24,18 +28,21 @@ class RepositorioBase(object):
             session.commit()
         return criado
     
+    @abstractmethod
     def alterar(self, editado: ModeloBase) -> ModeloBase:
         with orm.Session(self.engine, expire_on_commit=False) as session:
             session.add(editado)
             session.commit()
         return editado
     
+    @abstractmethod
     def remover(self, apagado: ModeloBase) -> ModeloBase:
         with orm.Session(self.engine, expire_on_commit=False) as session:
             session.delete(apagado)
             session.commit()
         return apagado
     
+    @abstractmethod
     def filtrar(self, **kwargs) -> List[ModeloBase]:
         with orm.Session(self.engine, expire_on_commit=False) as session:
             listados = session.execute(sa.select(
