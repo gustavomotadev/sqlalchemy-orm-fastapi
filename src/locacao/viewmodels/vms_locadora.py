991fd74ef2dca496cb4746d8a6755a2ff4af33a7
gustavomotadev <gustavomotadev@gmail.com>
2023-04-03 03:25:57 -0300

    pydantic validation for vms_locadora

--- src/locacao/viewmodels/vms_locadora.py
+++ src/locacao/viewmodels/vms_locadora.py
@@ -1,13 +1,24 @@
-import datetime
-from pydantic import BaseModel
+from datetime import time
+from uuid import uuid1
+from pydantic import BaseModel, Field, UUID1, validator
 from locacao.modelos.locadora import Locadora
 
+def _validar_horario(horario: time) -> time:
+    if horario < time(5) or horario > time(22) or int(horario.minute) % 15 != 0:
+        raise ValueError
+    else:
+        return horario
+
 class VMLocadora(BaseModel):
-    uuid: str
-    nome: str
-    horario_abertura: datetime.time
-    horario_fechamento: datetime.time
-    endereco: str
+    uuid: UUID1 = Field(default_factory=uuid1)
+    nome: str = Field(max_length=100)
+    horario_abertura: time
+    horario_fechamento: time
+    endereco: str = Field(max_length=255)
+
+    @validator('horario_abertura', 'horario_fechamento')
+    def validar_horario(horario: time) -> time: 
+        return _validar_horario(horario)
 
     @staticmethod
     def converter_modelo(modelo: Locadora):
@@ -17,11 +28,19 @@ class VMLocadora(BaseModel):
             endereco=modelo.endereco)
 
 class VMLocadoraSemUUID(BaseModel):
-    nome: str
-    horario_abertura: datetime.time
-    horario_fechamento: datetime.time
-    endereco: str
+    nome: str = Field(max_length=100)
+    horario_abertura: time
+    horario_fechamento: time
+    endereco: str = Field(max_length=255)
+
+    @validator('horario_abertura', 'horario_fechamento')
+    def validar_horario(horario: time) -> time:
+        return _validar_horario(horario)
 
 class VMLocadoraHorarios(BaseModel):
-    horario_abertura: datetime.time
-    horario_fechamento: datetime.time
+    horario_abertura: time
+    horario_fechamento: time
+
+    @validator('horario_abertura', 'horario_fechamento')
+    def validar_horario(horario: time) -> time:
+        return _validar_horario(horario)
